9cb7bf127bd1d4c406fbae5727714c8e2b1fc175
sumit0593 <sumitsumitsumit163@gmail.com>
2026-06-15 21:14:14 +0530

    fix routes

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,7 @@ import Google from "next-auth/providers/google"
 import Credentials from "next-auth/providers/credentials"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     GitHub({
       clientId: process.env.GITHUB_ID!,
